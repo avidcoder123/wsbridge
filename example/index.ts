0af2f8f9542f0d1c33c2cd8e55d189597e5302d7
avidcoder123 <umar.tli.edu@gmail.com>
2023-10-13 16:20:56 +0000

    todo

--- example/index.ts
+++ example/index.ts
@@ -2,6 +2,8 @@ import WebSocket from "ws";
 
 let port = 3145
 let ws = new WebSocket("ws://localhost:" + port)
+
+//TODO: FInd why this is not working
 ws.on("open", () => {
     ws.send("Kim Jong Il Mansae")
 })
